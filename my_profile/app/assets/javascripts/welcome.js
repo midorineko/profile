0025128a5404ef8d436a65ade303fec6e0688ad3
Steven Inouye <steven.inouye91@gmail.com>
2014-06-26 18:13:19 -0700

    initial commit

---
+++ my_profile/app/assets/javascripts/welcome.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+var welcome = ['W','e','l','c','o','m','e']
+// $(welcome).each(function(index){
+//   $('.welcome').append(welcome[index]);
+// });
+function intro() {
+  for(i=0; i < welcome.length; i++){
+     Welcome(i)
+  }
+}
+
+function Welcome(index){
+  $('.welcome').append(welcome[index]);
+}
+
+window.
+
+});
---
+++ my_profile/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+
+  root to: 'profile#home'
+
+end
