7b70adc7bddbcd6130826507ac6713b46d2eb2a5
Steven Inouye <steven.inouye91@gmail.com>
2014-06-28 00:52:20 -0700

    precompile

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20140628070844) do
 
   create_table "visitors", force: true do |t|
     t.string   "email"
+    t.string   "position"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
